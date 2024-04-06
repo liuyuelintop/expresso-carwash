f8aeeeb6e3b7e4cb2452d72f1b1065b5b4910fcd
liuyuelintop <liuyuelintop@gmail.com>
2024-04-06 13:16:00 +1100

    init the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="items-center ">
+      <h1 className="text-red-700 text-center">Expresso Carwash</h1>
+    </div>
+  )
+}
+
+export default App
